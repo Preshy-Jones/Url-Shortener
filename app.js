787ea6a687a4567c2942534727e4f19af33d31d5
Precious Adedibu <adedibuprecious@gmail.com>
2022-06-26 01:17:34 +0100

    updates

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const ejs = require("ejs");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
-const favicon = require("express-favicon");
 const path = require("path");
-app.use(favicon(__dirname + "/build/favicon.ico"));
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
